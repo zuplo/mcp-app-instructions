1c53cb78a0e2eb04d2d1cbc0c0a2a0f4ac55ee58
Nathan Totten <nate@zuplo.com>
2025-11-12 16:09:11 -0500

    Fix exports

--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-// Export both the function and the React component
-export { getMcpServerInstructions } from "./index";
-export { McpInstructions } from "./McpInstructions";
-export { App } from "./App";
